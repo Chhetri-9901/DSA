463ff08086e2a86d02218ece647b30a5a00b73e2
Chhetri-9901 <vivekchhetri9901@gmail.com>
2026-07-12 19:43:28 +0530

    move non zero numbers

--- arrays-easy--medium/moveNonZeroNumbers.js
+++ arrays-easy--medium/moveNonZeroNumbers.js
@@ -1,13 +1,13 @@
 // Move all zeroes to the end of an array, so that all values more than 0 are at front of all the zeros in an array.
 
-// let arr= [1, 0, 3, 0, 0, 2, 0, 5];
+let arr2= [1, 0, 3, 0, 0, 2, 0, 5];
 let arr= [0, 1, 0, 3, 12];
 
 let arrLen = arr.length;
 let p1 = 0;
 
 for(let i=0; i<arrLen; i++) {
-    if(i == p1 && arr[i] >0) {
+    if(i == p1 && arr[i] > 0) {
         arr[p1]  == arr[i];
         p1++
     }
@@ -19,3 +19,20 @@ for(let i=0; i<arrLen; i++) {
     }
 }
 console.log(arr);
+
+
+//Video Solution
+let arr2Len = arr2.length;
+let replacePoint = 0;
+
+for(let i=0; i<arr2Len; i++) {
+    if(arr2[i] !== 0) {
+        arr2[replacePoint] = arr2[i];
+        replacePoint++;
+    }
+}
+for(j=replacePoint; j<arr2Len; j++) {
+    arr2[j] = 0;
+}
+
+console.log(arr2);
